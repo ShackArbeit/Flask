667628ade207221eea354476063d115002aa59da
Shack Lin <g0972222165@gmail.com>
2023-12-21 11:42:50 +0800

    Add Zip Folder

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 # 引入 OS 模組
 import os
 # 從 Flask 中引入會使用到的物件
-from flask import Flask, flash, request, redirect, render_template, jsonify 
+from flask import Flask, flash, request, redirect, render_template, jsonify,send_file
 # 從 Werkzeug 中引入會使用到的物件
 from werkzeug.utils import secure_filename
-
+# 從 shp.py 檔案中引入  Shp_To_GeoJson 函式，用於轉檔用的
 from shp import Shp_To_GeoJson;
-
+# 從 shp.py 檔案中引入  Read_GeoJson 函式，用於返回轉檔後的檔案的
 from shp import Read_GeoJson;
+import zipfile
+from io import BytesIO
+
+
+
 
 # 初始化 Flask 模組
 app = Flask( __name__, static_folder="static",static_url_path='/static')
@@ -61,18 +66,53 @@ def upload_file():
         return redirect('/')
 
 
-
+# 這裡為將 Shp 檔案轉檔的路由設定
 @app.route('/shp', methods=['POST'])
 def shp_to_geojson():
     Shp_To_GeoJson()
     flash('.shp 檔案已經成功轉為 GEJSON 檔了 !，請查看')
     return redirect('/')
 
+# 這裡為將轉成 geojson 的檔案返回給前端的路由設定
 @app.route('/getShp')
 def get_geojson():
      geojson_contents =Read_GeoJson()
-     print(geojson_contents)
      return jsonify(geojson_contents)
+
+from flask import send_file
+
+# 這裡為讓使用者可以從前端將 geojson 檔案下載下來的路由設定
+@app.route('/download')
+def DownLoad():
+     # 取得當前檔案下的路徑
+    current_path = os.getcwd()
+    # 所有 GeoJSON 檔案所在的資料夾路徑
+    geojson_folder_path = current_path
+    download_files_array=[]
+    zip_buffer = BytesIO()
+
+    # 將所有的 .geojson 檔案透過 Iterate 方法遍歷一次
+    for filename in os.listdir(geojson_folder_path):
+        if filename.endswith('.geojson'):
+            # 在遍歷的時候要定義每一個 geojson 檔案的當前路徑
+            file_path = os.path.join(geojson_folder_path, filename)
+            download_files_array.append(filename)
+            with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as zip_file:
+                 for file in download_files_array:
+                      zip_file.write(file)
+            print("已下載的 GeoJSON 檔案如下 : ")
+            print(file_path)
+            print(filename)
+    zip_buffer.seek(0)
+
+    # 返回zip文件到前端
+    return send_file(zip_buffer, download_name='files.zip', as_attachment=True)
+
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 
 
 # 在本地端 POST=5000運行
--- shp.py
+++ shp.py
@@ -2,7 +2,9 @@
 import geopandas as gpd
 # 引入 OS 模組
 import os
+# 引入 Json 物件
 import json
+from flask import send_file
 
 os.environ['SHAPE_RESTORE_SHX'] = 'YES'
 # 取得當前檔案下的路徑
@@ -57,3 +59,26 @@ def Read_GeoJson():
         print(f"File: {filename}")
         print(json_content)
     return geojson_contents_dict
+
+
+
+def Download_GeoJosn():
+    # 取得當前檔案下的路徑
+    current_path = os.getcwd()
+    # 所有 GeoJSON 檔案所在的資料夾路徑
+    geojson_folder_path = current_path
+
+    # 定義空的字典，用於儲存所有副檔名有 .geojson 的檔案的內容
+    geojson_contents_dict = {}
+
+    # 將所有的 .geojson 檔案透過 Iterate 方法遍歷一次
+    for filename in os.listdir(geojson_folder_path):
+        if filename.endswith('.geojson'):
+            # 在遍歷的時候要定義每一個 geojson 檔案的當前路徑
+            file_path = os.path.join(geojson_folder_path, filename)
+            print("已下載的 GeoJSON 檔案如下 : ")
+            print(file_path)
+            print(filename)
+            return send_file(file_path, as_attachment=True)
+
+    
